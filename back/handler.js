424ba5414ce5a17aaa003fef35dbc92464e3e326
JZylber <zylberjulian@gmail.com>
2024-08-05 14:05:08 -0300

    feat: realtime example

--- back/handler.js
+++ back/handler.js
@@ -1,13 +1,17 @@
-import { onEvent, startServer } from "soquetic";
+import { onEvent, sendEvent, startServer } from "soquetic";
 
 onEvent("message", (data) => {
-    console.log(`Mensaje recibido: ${data.msg}`);
-    return { msg: `Mensaje recibido: ${data.msg}` };
+  console.log(`Mensaje recibido: ${data.msg}`);
+  return { msg: `Mensaje recibido: ${data.msg}` };
 });
 
 onEvent("date", () => {
-    const date = new Date();
-    return `${date.getUTCDate()}/${date.getUTCMonth() + 1}`;
+  const date = new Date();
+  return `${date.getUTCDate()}/${date.getUTCMonth() + 1}`;
 });
+
+setInterval(() => {
+  sendEvent("second", null);
+}, 1000);
 
 startServer();
--- front/script.js
+++ front/script.js
@@ -2,6 +2,7 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const a = document.getElementById("a");
 const date = document.getElementById("fecha");
+const seconds = document.getElementById("segundos");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -15,4 +16,8 @@ form.addEventListener("submit", (e) => {
 
 fetchData("date", (data) => {
   date.innerText = data;
+});
+
+receive("second", () => {
+  seconds.innerText = parseInt(seconds.innerText) + 1;
 });
