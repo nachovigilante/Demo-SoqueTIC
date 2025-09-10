bfab4dc394bd83eef7b6f52c4888bcfd7687e9c8
JZylber <zylberjulian@gmail.com>
2025-09-10 11:23:11 -0300

    feat(backend): docs

--- back/handler.js
+++ back/handler.js
@@ -1,3 +1,4 @@
+// Importa las funciones de la librería soquetic
 import {
   subscribeGETEvent,
   subscribePOSTEvent,
@@ -5,18 +6,46 @@ import {
   startServer,
 } from "soquetic";
 
-subscribePOSTEvent("message", (data) => {
+/**
+ * Maneja un evento POST de nombre "mensaje", console logueando el mensaje recibido y devolviendo un objeto como respuesta.
+ *
+ * @param {Object} data - El objeto de datos que contiene el mensaje.
+ * @param {string} data.msg - El mensaje recibido.
+ * @returns {Object} Un objeto que contiene el mensaje de confirmación (en el atributo msg).
+ */
+function messageEventHandler(data) {
   console.log(`Mensaje recibido: ${data.msg}`);
   return { msg: `Mensaje recibido: ${data.msg}` };
-});
+}
 
-subscribeGETEvent("date", () => {
+/**
+ *  Maneja un evento "GET" de nombre "date". Devuelve la fecha y el mes actual como un string en el formato "día/mes".
+ *
+ * @returns {string} El día y mes actual (por ejemplo, "15/6").
+ */
+function dateEventHandler() {
   const date = new Date();
   return `${date.getUTCDate()}/${date.getUTCMonth() + 1}`;
-});
+}
 
-setInterval(() => {
+/**
+ *  Función para enviar un evento "RealTime" de nombre "second", sin datos adicionales.
+ *
+ * @function
+ * @returns {void}
+ */
+function sendSecond() {
   realTimeEvent("second", null);
-}, 1000);
+}
 
+// Suscribe el evento POST "message" y lo maneja con la función messageEventHandler
+subscribePOSTEvent("message", messageEventHandler);
+
+// Suscribe el evento GET "date" y lo maneja con la función dateEventHandler
+subscribeGETEvent("date", dateEventHandler);
+
+// Envía el evento "second" cada segundo usando la función sendSecond
+setInterval(sendSecond, 1000);
+
+// Inicia el servidor
 startServer();
